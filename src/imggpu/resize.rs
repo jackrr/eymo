3021a9f55afc1c7b6b746b9bac777df23a0635d0
Jack Ratner <jackratner@gmail.com>
2025-06-15 20:11:19 -0400

    wip -- resize from buffer
    circumvent slow rbg -> rgba conversion by implementing byte-level
    operations on a buffer in gpu layer

--- src/imggpu/resize.rs
+++ src/imggpu/resize.rs
@@ -1,6 +1,7 @@
 use image::{RgbImage, RgbaImage, DynamicImage};
 use pollster::FutureExt;
 use wgpu::{wgt::SamplerDescriptor, AddressMode, Device, FilterMode, Queue, SamplerBindingType, TextureDescriptor};
+use tracing::{span, Level};
 
 pub enum ResizeAlgo {
     Nearest,
@@ -29,7 +30,22 @@ fn int_div_round_up(divisor: u32, dividend: u32) -> u32 {
 	}
 }
 
+// ~65-100ms
+fn rgb_to_rgba_bytes(img: &RgbImage) -> Vec<u8> {
+    let rgba_bytes = (img.width() * img.height() * 4) as usize;
+    let mut rgba_raw: Vec<u8> = vec![0; rgba_bytes];
+    for (src, dest) in img.as_raw().chunks_exact(3).zip(rgba_raw.chunks_exact_mut(4)) {
+        dest[..3].copy_from_slice(src);
+    }
+    rgba_raw
+}
+
+// TODO: cache wgpu init
+// TODO: cache texture init somehow (1 per I/O size)
+// TODO: skip alpha conversion somehow
+
 async fn init_wgpu() -> (Device, Queue) {
+    
     // TODO: less unwrap, more proper error bubbling
     let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
 			  backends: wgpu::Backends::all(),
@@ -50,17 +66,35 @@ async fn init_wgpu() -> (Device, Queue) {
     return (device, queue)
 }
 
+// Goal: 10ms total runtime
 pub fn resize(img: &RgbImage, width: u32, height: u32, algo: ResizeAlgo) -> RgbImage {
-    // TODO: Avoid costly? alpha conversions
-    let img = DynamicImage::ImageRgb8(img.clone()).into_rgba8();
+    let span = span!(Level::INFO, "resize");
+    let _guard = span.enter();
 
+    // 80-120ms
+    // TODO: Avoid costly alpha conversions
+    let alpha_span = span!(Level::INFO, "resize_convert_to_alpha");
+    let alpha_guard = alpha_span.enter();
+
+    // Roughly same perf...
+    // let img_raw_bytes = rgb_to_rgba_bytes(img);
+    // let img = DynamicImage::ImageRgb8(img.clone()).into_rgba8();
+    drop(alpha_guard);
+
+    // 30-40ms
+    let init_span = span!(Level::INFO, "resize_init_gpu");
+    let init_guard = init_span.enter();
     let (device, queue) = init_wgpu().block_on();
-    
-    let input_texture_size = wgpu::Extent3d {
-        width: img.width(),
-        height: img.height(),
-        depth_or_array_layers: 1,
-    };
+    drop(init_guard);
+
+    // 14ms
+    let textures_span = span!(Level::INFO, "resize_textures");
+    let textures_guard = textures_span.enter();
+    // let input_texture_size = wgpu::Extent3d {
+    //     width: img.width(),
+    //     height: img.height(),
+    //     depth_or_array_layers: 1,
+    // };
 
     let output_texture_size = wgpu::Extent3d {
         width,
@@ -68,30 +102,54 @@ pub fn resize(img: &RgbImage, width: u32, height: u32, algo: ResizeAlgo) -> RgbI
         depth_or_array_layers: 1,
     };
 
-    let input_texture = device.create_texture(
-        &TextureDescriptor {
-            label: Some("input image"),
-            size: input_texture_size,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8Unorm,
-            view_formats: &[wgpu::TextureFormat::Rgba8Unorm],
-            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST
-        }
-    );
+    // let input_texture = device.create_texture(
+    //     &TextureDescriptor {
+    //         label: Some("input image"),
+    //         size: input_texture_size,
+    //         mip_level_count: 1,
+    //         sample_count: 1,
+    //         dimension: wgpu::TextureDimension::D2,
+    //         format: wgpu::TextureFormat::Rgba8Unorm,
+    //         view_formats: &[wgpu::TextureFormat::Rgba8Unorm],
+    //         usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST
+    //     }
+    // );
 
-    queue.write_texture(
-        input_texture.as_image_copy(),
-        img.as_raw(),
-        wgpu::TexelCopyBufferLayout {
-            offset: 0,
-            bytes_per_row: Some(4*img.width()),
-            rows_per_image: None,
-        },
-        input_texture_size);
+    // queue.write_texture(
+    //     input_texture.as_image_copy(),
+    //     img.as_raw(),
+    //     // &img_raw_bytes,
+    //     wgpu::TexelCopyBufferLayout {
+    //         offset: 0,
+    //         bytes_per_row: Some(4*img.width()),
+    //         rows_per_image: None,
+    //     },
+    //     input_texture_size);
+		let input_buf = device.create_buffer(&wgpu::BufferDescriptor {
+			  label: Some("input_buf"),
+        // w x h x rgb
+			  size: (img.width() * img.height() * 3) as u64,
+			usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::STORAGE,
+			mapped_at_creation: false
+		});
 
+		let width_uniform = device.create_buffer(&wgpu::BufferDescriptor {
+			label: Some("width_uniform"),
+			size: 4,
+			usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+			mapped_at_creation: false
+		});
 
+		let height_uniform = device.create_buffer(&wgpu::BufferDescriptor {
+			label: Some("height_uniform"),
+			size: 4,
+			usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+			mapped_at_creation: false
+		});
+
+    queue.write_buffer(&input_buf, 0, img.as_raw());
+    queue.write_buffer(&width_uniform, 0, &img.width().to_ne_bytes());
+    queue.write_buffer(&height_uniform, 0, &img.height().to_ne_bytes());
 
     // let mut sampler_descriptor = SamplerDescriptor::default();
     // sampler_descriptor.min_filter = FilterMode::Linear;
@@ -114,46 +172,17 @@ pub fn resize(img: &RgbImage, width: u32, height: u32, algo: ResizeAlgo) -> RgbI
         }
     );
 
+    drop(textures_guard);
+
+    // 5ms
+    let load_shader_span = span!(Level::INFO, "resize_load_shader");
+    let load_shader_guard = load_shader_span.enter();
     let shader = device.create_shader_module(wgpu::include_wgsl!("resize.wgsl"));
+    drop(load_shader_guard);
 
-    // let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-    //     label: Some("bind group layout"),
-    //     entries: &[
-    //         wgpu::BindGroupLayoutEntry {
-    //             binding: 0,
-    //             visibility: wgpu::ShaderStages::COMPUTE,
-    //             ty: wgpu::BindingType::Texture {
-    //                 sample_type: wgpu::TextureSampleType::default(),
-    //                 view_dimension: wgpu::TextureViewDimension::D2,
-    //                 multisampled: false
-    //             },
-    //             count: None
-    //         },
-    //         wgpu::BindGroupLayoutEntry {
-    //             binding: 1,
-    //             visibility: wgpu::ShaderStages::COMPUTE,
-    //             ty: wgpu::BindingType::Sampler(SamplerBindingType::Filtering),
-    //             count: None,
-    //         },
-    //         wgpu::BindGroupLayoutEntry {
-    //             binding: 2,
-    //             visibility: wgpu::ShaderStages::COMPUTE,
-    //             ty: wgpu::BindingType::Texture {
-    //                 sample_type: wgpu::TextureSampleType::default(),
-    //                 view_dimension: wgpu::TextureViewDimension::D2,
-    //                 multisampled: false
-    //             },
-    //             count: None
-    //         },
-    //     ]
-    // });
-
-    // let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-		// 	label: Some("pipeline_layout"),
-		// 	bind_group_layouts: &[&bind_group_layout],
-		// 	push_constant_ranges: &[]
-		// });
-
+    // 1ms
+    let compute_prep_span = span!(Level::INFO, "resize_compute_prep");
+    let compute_prep_guard = compute_prep_span.enter();
 		let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
 			  label: Some("pipeline"),
 			  layout: None,
@@ -167,10 +196,15 @@ pub fn resize(img: &RgbImage, width: u32, height: u32, algo: ResizeAlgo) -> RgbI
 			  label: Some("bind_group"),
 			  layout: &pipeline.get_bind_group_layout(0),
 			  entries: &[
-				    wgpu::BindGroupEntry {
-					      binding: 0,
-					      resource: wgpu::BindingResource::TextureView(&input_texture.create_view(&wgpu::TextureViewDescriptor::default())),
-				    },
+				    // wgpu::BindGroupEntry {
+					  //     binding: 0,
+					  //     resource: wgpu::BindingResource::TextureView(&input_texture.create_view(&wgpu::TextureViewDescriptor::default())),
+				    // },
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: input_buf.as_entire_binding(),
+            },
+            
 				    // wgpu::BindGroupEntry {
 					  //     binding: 1,
 					  //     resource: wgpu::BindingResource::Sampler(&sampler),
@@ -178,7 +212,15 @@ pub fn resize(img: &RgbImage, width: u32, height: u32, algo: ResizeAlgo) -> RgbI
 				    wgpu::BindGroupEntry {
 					      binding: 1,
 					      resource:  wgpu::BindingResource::TextureView(&output_texture.create_view(&wgpu::TextureViewDescriptor::default())),
-				    }
+				    },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: width_uniform.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 3,
+                resource: height_uniform.as_entire_binding(),
+            },
 			  ]
 		});
 
@@ -187,6 +229,11 @@ pub fn resize(img: &RgbImage, width: u32, height: u32, algo: ResizeAlgo) -> RgbI
 			  label: Some("encoder")
 		});
 
+    drop(compute_prep_guard);
+
+    // 5ms (nearest, tbd linear)
+    let gpu_run_span = span!(Level::INFO, "resize_gpu_run");
+    let gpu_run_guard = gpu_run_span.enter();
     let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
         label: Some("compute"),
         timestamp_writes: None
@@ -237,7 +284,11 @@ pub fn resize(img: &RgbImage, width: u32, height: u32, algo: ResizeAlgo) -> RgbI
     buffer_slice.map_async(wgpu::MapMode::Read, |r| r.unwrap());
 
     device.poll(wgpu::PollType::Wait).unwrap();
+    drop(gpu_run_guard);
 
+    // 1ms
+    let extract_image_span = span!(Level::INFO, "resize_extract_image");
+    let extract_image_guard = extract_image_span.enter();
     let padded_data = buffer_slice.get_mapped_range();
 
     let mut pixels: Vec<u8> = vec![0; unpadded_bytes_per_row * height as usize];
@@ -246,7 +297,15 @@ pub fn resize(img: &RgbImage, width: u32, height: u32, algo: ResizeAlgo) -> RgbI
         .zip(pixels.chunks_exact_mut(unpadded_bytes_per_row)) {
             pixels.copy_from_slice(&padded[..unpadded_bytes_per_row]);
         }
+    drop(extract_image_guard);
 
+    // 1-2ms
+    let alpha_to_rgb_span = span!(Level::INFO, "resize_alpha_to_rgb");
+    let alpha_to_rgb_guard = alpha_to_rgb_span.enter();
+    
     let with_alpha = RgbaImage::from_raw(width, height, pixels).unwrap();
-    DynamicImage::ImageRgba8(with_alpha).to_rgb8()
+    let res = DynamicImage::ImageRgba8(with_alpha).to_rgb8();
+    drop(alpha_to_rgb_guard);
+
+    res
 }
--- src/imggpu/resize.wgsl
+++ src/imggpu/resize.wgsl
@@ -1,7 +1,48 @@
-@group(0) @binding(0) var input_texture : texture_2d<f32>;
+// @group(0) @binding(0) var input_texture : texture_2d<f32>;
 // @group(0) @binding(1) var input_sampler : sampler;
+@group(0) @binding(0) var<storage, read> input_buf : array<u32>;
 @group(0) @binding(1) var output_texture
     : texture_storage_2d<rgba8unorm, write>;
+@group(0) @binding(2) var<uniform> width : u32;
+@group(0) @binding(3) var<uniform> height : u32;
+
+// To get 3 bytes starting at byte index `byte_index`:
+fn get_3_bytes(byte_index : u32) -> vec3<u32> {
+  let word_index = byte_index / 4u;
+  let byte_offset = byte_index % 4u;
+
+  let word1 = input_buf[word_index];
+  let word2 = input_buf[word_index + 1u];
+
+  // Extract bytes using bit operations
+  var bytes : vec3<u32>;
+
+  if (byte_offset == 0u) {
+    bytes.x = word1 & 0xFFu;
+    bytes.y = (word1 >> 8u) & 0xFFu;
+    bytes.z = (word1 >> 16u) & 0xFFu;
+  } else if (byte_offset == 1u) {
+    bytes.x = (word1 >> 8u) & 0xFFu;
+    bytes.y = (word1 >> 16u) & 0xFFu;
+    bytes.z = (word1 >> 24u) & 0xFFu;
+  } else if (byte_offset == 2u) {
+    bytes.x = (word1 >> 16u) & 0xFFu;
+    bytes.y = (word1 >> 24u) & 0xFFu;
+    bytes.z = word2 & 0xFFu;
+  } else { // byte_offset == 3u
+    bytes.x = (word1 >> 24u) & 0xFFu;
+    bytes.y = word2 & 0xFFu;
+    bytes.z = (word2 >> 8u) & 0xFFu;
+  }
+
+  return bytes;
+}
+
+fn get_rgba(coords : vec2<u32>) -> vec4<f32> {
+  let byte_offset = coords.y * height * width + coords.x * width;
+  let rgb = get_3_bytes(byte_offset);
+  return vec4<f32>(f32(rgb.x), f32(rgb.y), f32(rgb.z), 1.0);
+}
 
 // Manual bilinear interpolation for compute shaders
 fn bilinear_sample(tex : texture_2d<f32>, uv : vec2<f32>) -> vec4<f32> {
@@ -40,36 +81,38 @@ fn bilinear_sample(tex : texture_2d<f32>, uv : vec2<f32>) -> vec4<f32> {
 }
 
 // Compute shader for image resizing
-@compute @workgroup_size(8, 8) fn
-    resize_image_sampler(@builtin(global_invocation_id) global_id : vec3<u32>) {
-  let output_size = textureDimensions(output_texture);
-  let coords = vec2<u32>(global_id.xy);
+// @compute @workgroup_size(8, 8) fn
+//     resize_image_sampler(@builtin(global_invocation_id) global_id :
+//     vec3<u32>) {
+//   let output_size = textureDimensions(output_texture);
+//   let coords = vec2<u32>(global_id.xy);
 
-  // Early exit if outside bounds
-  if (coords.x >= output_size.x || coords.y >= output_size.y) {
-    return;
-  }
+//   // Early exit if outside bounds
+//   if (coords.x >= output_size.x || coords.y >= output_size.y) {
+//     return;
+//   }
 
-  // Calculate UV coordinates for the input texture
-  let uv = (vec2<f32>(coords) + 0.5) / vec2<f32>(output_size);
+//   // Calculate UV coordinates for the input texture
+//   let uv = (vec2<f32>(coords) + 0.5) / vec2<f32>(output_size);
 
-  // TODO: switch to fragment shader for hardware sampling
-  // let color = textureSample(input_texture, input_sampler, uv);
-  let color = bilinear_sample(input_texture, uv);
+//   // TODO: switch to fragment shader for hardware sampling
+//   // let color = textureSample(input_texture, input_sampler, uv);
+//   // let color = bilinear_sample(input_texture, uv);
 
-  // Write to output texture
-  textureStore(output_texture, coords, color);
-}
+//   // Write to output texture
+//   textureStore(output_texture, coords, color);
+// }
 
 @compute @workgroup_size(8, 8) fn
     resize_image_nearest(@builtin(global_invocation_id) global_id : vec3<u32>) {
   let output_size = textureDimensions(output_texture);
-  let input_size = vec2<f32>(textureDimensions(input_texture));
+  // let input_size = vec2<f32>(textureDimensions(input_texture));
   let coords = vec2<i32>(global_id.xy);
 
   if (coords.x >= i32(output_size.x) || coords.y >= i32(output_size.y)) {
     return;
   }
+  let input_size = vec2<f32>(vec2<u32>(width, height));
 
   // Calculate corresponding input coordinates
   let scale = input_size / vec2<f32>(output_size);
@@ -77,9 +120,10 @@ fn bilinear_sample(tex : texture_2d<f32>, uv : vec2<f32>) -> vec4<f32> {
 
   // Clamp to texture bounds
   let clamped_coords =
-      clamp(input_coords, vec2<i32>(0), vec2<i32>(input_size) - 1);
+      vec2<u32>(clamp(input_coords, vec2<i32>(0), vec2<i32>(input_size) - 1));
 
   // Sample and store
-  let color = textureLoad(input_texture, clamped_coords, 0);
+  // let color = textureLoad(input_texture, clamped_coords, 0);
+  let color = get_rgba(clamped_coords);
   textureStore(output_texture, coords, color);
 }
